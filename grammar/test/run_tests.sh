dc2a83559735e994be8b97c739905490d7da1d0b
Iain Waugh <iwaugh@gmail.com>
2022-10-01 15:29:47 +1000

    Add a test for 'process' and 'if' and tweak the PEG to pass

--- grammar/test/run_tests.sh
+++ grammar/test/run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Testing 'literal's"
+echo "Testing section 9.3.2 : literals"
 ../peglint.exe ../vhdl2008.peg --trace test_9.3.2_literal.vhd > result_9.3.2_literal.txt
+
+echo "Testing section 11.3  : process"
+../peglint.exe ../vhdl2008.peg --trace test_11.3_process.vhd > result_11.3_process.txt
---
+++ grammar/test/test_11.3_process.vhd
@@ -0,0 +1,47 @@
+-------------------------------------------------------------------------------
+--
+-- Copyright (c) 2022 Iain Waugh
+-- All rights reserved.
+--
+-- Non-functional VHDL code to test the PEG
+--
+-- Test VHDL-2008
+--   Covers:
+--     Section 11.3 - process
+--     Section 10.3 - if
+--
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+architecture test of vhdl_process is
+begin
+
+  process (pixel_clk)
+  begin
+    if (rising_edge(pixel_clk)) then
+      if (frame_sync_local = '1') then
+        pixel_red   <= (others => '0');
+        pixel_green <= (others => '0');
+        pixel_blue  <= (others => '0');
+      else
+        if (pixel_in_ready = '1') then
+          pixel_red <= pixel_red + 1;
+        elsif (pixel_in_ready = 'Z') then
+          null;
+        else
+
+          if (pixel_red = unsigned(all_ones(C_BITS_RED))) then
+            pixel_green <= pixel_green + 1;
+
+            if (pixel_green = unsigned(all_ones(C_BITS_GREEN))) then
+              pixel_blue <= pixel_blue + 1;
+            end if;
+          end if;
+        end if;
+      end if;
+    end if;
+  end process;
+  
+end test;
