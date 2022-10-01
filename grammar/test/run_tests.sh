948786c6f9d3fecf6dc78106e335cc165cce2289
Iain Waugh <iwaugh@gmail.com>
2022-10-01 15:34:30 +1000

    Switch to AST peglint output, not full debug

--- grammar/test/run_tests.sh
+++ grammar/test/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Testing section 9.3.2 : literals"
-../peglint.exe ../vhdl2008.peg --trace test_9.3.2_literal.vhd > result_9.3.2_literal.txt
+../peglint.exe ../vhdl2008.peg --ast test_9.3.2_literal.vhd > result_9.3.2_literal.txt
 
 echo "Testing section 11.3  : process"
-../peglint.exe ../vhdl2008.peg --trace test_11.3_process.vhd > result_11.3_process.txt
+../peglint.exe ../vhdl2008.peg --ast test_11.3_process.vhd > result_11.3_process.txt
