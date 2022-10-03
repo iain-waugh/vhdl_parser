71264d9880c7084d64a55454d5e48d373a650456
Iain Waugh <iwaugh@gmail.com>
2022-10-03 21:42:06 +1100

    Trim leading spaces in the test summaries

--- grammar/test/run_tests.sh
+++ grammar/test/run_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Testing section 9.3.2 : literals"
 ../peglint.exe ../vhdl2008.peg --ast test_9.3.2_literal.vhd > result_9.3.2_literal.txt
-grep - result_9.3.2_literal.txt > summary_9.3.2_literal.txt
+grep - result_9.3.2_literal.txt | sed -e 's/^[ \t]*//' > summary_9.3.2_literal.txt
 
 echo "Testing section 10.5  : signal assignment"
 ../peglint.exe ../vhdl2008.peg --ast test_10.5_signal_assignment.vhd > result_10.5_signal_assignment.txt
-grep - result_10.5_signal_assignment.txt > summary_10.5_signal_assignment.txt
+grep - result_10.5_signal_assignment.txt | sed -e 's/^[ \t]*//' > summary_10.5_signal_assignment.txt
 
 echo "Testing section 11.3  : process"
 ../peglint.exe ../vhdl2008.peg --ast test_11.3_process.vhd > result_11.3_process.txt
-grep - result_11.3_process.txt > summary_11.3_process.txt
+grep - result_11.3_process.txt | sed -e 's/^[ \t]*//'> summary_11.3_process.txt
