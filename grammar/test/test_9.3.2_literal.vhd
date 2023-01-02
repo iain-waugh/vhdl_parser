51b4f8346fbc66de439cfdd9eab5ea6edd03bdfe
Iain Waugh <iwaugh@gmail.com>
2023-01-02 17:16:46 +1100

    Fix std_logic_textio issue where string literal has a lower case 'x'

--- grammar/test/test_9.3.2_literal.vhd
+++ grammar/test/test_9.3.2_literal.vhd
@@ -53,21 +53,21 @@ begin
   sig33 <= X1;
   sig34 <= c_out;
   sig35 <= STORE_NEXT_ITEM;
-  sig36 <= intra; -- Clash with 'in' keyword?
+  sig36 <= intra;                       -- Clash with 'in' keyword?
 
 -- extended_identifier
   sig41 <= \BUS\;
   sig42 <= \bus\;
   sig43 <= \a\\b\;
-  
+
 -- character_literal
   sig51 <= 'A';
   sig52 <= '*';
   sig53 <= ''';
   sig54 <= ' ';
-  
+
 -- string_literal
-  sig61 <= "Setup time is too short"; -- An error message.
+  sig61 <= "Setup time is too short";   -- An error message.
   sig62 <= "";
   sig63 <= " ";
   sig64 <= "A";
@@ -75,25 +75,26 @@ begin
   sig66 <= "Characters such as $, %, and } are allowed in string literals.";
   sig67 <= "FIRST PART OF A SEQUENCE OF CHARACTERS " &
            "THAT CONTINUES ON THE NEXT LINE";
-    
+
   -- bit_string_literal
-  sig71 <= B"1111_1111_1111"; -- Equivalent to the string literal "111111111111".
-  sig72 <= X"FFF"; -- Equivalent to B"1111_1111_1111".
-  sig73 <= O"777"; -- Equivalent to B"111_111_111".
-  sig74 <= X"777"; -- Equivalent to B"0111_0111_0111".
-  sig75 <= B"XXXX_01LH"; -- Equivalent to the string literal "XXXX01LH"
-  sig76 <= UO"27"; -- Equivalent to B"010_111"
-  sig77 <= UO"2C"; -- Equivalent to B"011_CCC"
-  sig78 <= SX"3W"; -- Equivalent to B"0011_WWWW"
-  sig79 <= D"35"; -- Equivalent to B"100011"
-  sig7a <= 12UB"X1"; -- Equivalent to B"0000_0000_00X1"
-  sig7b <= 12SB"X1"; -- Equivalent to B"XXXX_XXXX_XXX1"
-  sig7c <= 12UX"F-"; -- Equivalent to B"0000_1111_----"
-  sig7d <= 12SX"F-"; -- Equivalent to B"1111_1111_----"
-  sig7e <= 12D"13"; -- Equivalent to B"0000_0000_1101"
-  sig7f <= 12UX"000WWW"; -- Equivalent to B"WWWW_WWWW_WWWW"
-  sig7g <= 12SX"FFFC00"; -- Equivalent to B"1100_0000_0000"
-  sig7h <= 12SX"XXXX00"; -- Equivalent to B"XXXX_0000_0000"
+  sig71  <= B"1111_1111_1111";  -- Equivalent to the string literal "111111111111".
+  sig72a <= X"FFF";                     -- Equivalent to B"1111_1111_1111".
+  sig72b <= x"fFf";                     -- Equivalent to B"1111_1111_1111".
+  sig73  <= O"777";                     -- Equivalent to B"111_111_111".
+  sig74  <= X"777";                     -- Equivalent to B"0111_0111_0111".
+  sig75  <= B"XXXX_01LH";       -- Equivalent to the string literal "XXXX01LH"
+  sig76  <= UO"27";                     -- Equivalent to B"010_111"
+  sig77  <= UO"2C";                     -- Equivalent to B"011_CCC"
+  sig78  <= SX"3W";                     -- Equivalent to B"0011_WWWW"
+  sig79  <= D"35";                      -- Equivalent to B"100011"
+  sig7a  <= 12UB"X1";                   -- Equivalent to B"0000_0000_00X1"
+  sig7b  <= 12SB"X1";                   -- Equivalent to B"XXXX_XXXX_XXX1"
+  sig7c  <= 12UX"F-";                   -- Equivalent to B"0000_1111_----"
+  sig7d  <= 12SX"F-";                   -- Equivalent to B"1111_1111_----"
+  sig7e  <= 12D"13";                    -- Equivalent to B"0000_0000_1101"
+  sig7f  <= 12UX"000WWW";               -- Equivalent to B"WWWW_WWWW_WWWW"
+  sig7g  <= 12SX"FFFC00";               -- Equivalent to B"1100_0000_0000"
+  sig7h  <= 12SX"XXXX00";               -- Equivalent to B"XXXX_0000_0000"
 
 -- _null
   sig81 <= null;
