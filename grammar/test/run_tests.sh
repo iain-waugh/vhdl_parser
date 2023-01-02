1bf33a9165782135a9315ada7f810c849e6a6f73
Iain Waugh <iwaugh@gmail.com>
2023-01-02 17:17:13 +1100

    Save test results in a 'results' folder

--- grammar/test/run_tests.sh
+++ grammar/test/run_tests.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+mkdir -p results
+
 echo "Testing section 9.3.2 : literals"
-../peglint ../vhdl2008.peg --packrat --ast test_9.3.2_literal.vhd > result_9.3.2_literal.txt
-grep - result_9.3.2_literal.txt | sed -e 's/^[ \t]*//' > summary_9.3.2_literal.txt
+../peglint ../vhdl2008.peg --packrat --ast test_9.3.2_literal.vhd > results/result_9.3.2_literal.txt
+grep - results/result_9.3.2_literal.txt | sed -e 's/^[ \t]*//' > results/summary_9.3.2_literal.txt
 
 echo "Testing section 10.5  : signal assignment"
-../peglint ../vhdl2008.peg --packrat --ast test_10.5_signal_assignment.vhd > result_10.5_signal_assignment.txt
-grep - result_10.5_signal_assignment.txt | sed -e 's/^[ \t]*//' > summary_10.5_signal_assignment.txt
+../peglint ../vhdl2008.peg --packrat --ast test_10.5_signal_assignment.vhd > results/result_10.5_signal_assignment.txt
+grep - results/result_10.5_signal_assignment.txt | sed -e 's/^[ \t]*//' > results/summary_10.5_signal_assignment.txt
 
 echo "Testing section 4.7   : package"
-../peglint ../vhdl2008.peg --packrat --ast test_4.7_package.vhd > result_4.7_package.txt
-grep - result_4.7_package.txt | sed -e 's/^[ \t]*//'> summary_4.7_package.txt
+../peglint ../vhdl2008.peg --packrat --ast test_4.7_package.vhd > results/result_4.7_package.txt
+grep - results/result_4.7_package.txt | sed -e 's/^[ \t]*//'> results/summary_4.7_package.txt
 
 echo "Testing section 11.3  : process"
-../peglint ../vhdl2008.peg --packrat --ast test_11.3_process.vhd > result_11.3_process.txt
-grep - result_11.3_process.txt | sed -e 's/^[ \t]*//'> summary_11.3_process.txt
+../peglint ../vhdl2008.peg --packrat --ast test_11.3_process.vhd > results/result_11.3_process.txt
+grep - results/result_11.3_process.txt | sed -e 's/^[ \t]*//'> results/summary_11.3_process.txt
