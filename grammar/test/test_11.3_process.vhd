3d45dffe70f666598c0f4e596a33d5ab210c04ea
Iain Waugh <iwaugh@gmail.com>
2023-01-29 16:51:17 +1100

    Fix an issue with "if (A_MSB < B_LSB or B_MSB < A_LSB) then"

--- grammar/test/test_11.3_process.vhd
+++ grammar/test/test_11.3_process.vhd
@@ -19,31 +19,32 @@ architecture test of vhdl_process is
 begin
 
   process (pixel_clk)
-    variable result : UNSIGNED (A'range);
-    variable L01 : UNSIGNED(SIZE-1 downto 0);
+    variable result : unsigned (A'range);
+    variable L01    : UNSIGNED(SIZE-1 downto 0);
   begin
     if (rising_edge(pixel_clk)) then
-      result := (others => 'X');
+      result := (others     => 'X');
       result := L01'(others => 'X');
 
-      if (frame_sync_local >= 1) then
-        pixel_red   <= (others => '0');
-      else
-        if (pixel_in_ready = '1') then
-          pixel_red <= pixel_red + 1;
-        elsif (pixel_in_ready = 'Z') then
-          null;
-        else
-
-          my_if : if (pixel_red = unsigned(all_ones(C_BITS_RED))) then
-            pixel_green <= pixel_green + 1;
-
-            if (pixel_green = unsigned(all_ones(C_BITS_GREEN))) then
-              pixel_blue <= pixel_blue + 1;
-            end if;
-          end if my_if;
-        end if;
+      if (A_MSB < B_LSB or B_MSB < A_LSB) then
+        pixel_red <= (others => '0');
       end if;
+
+      if(frame_sync_local >= 1) then
+        pixel_red <= pixel_red + 1;
+      elsif (pixel_in_ready = 'Z') then
+        null;
+      else
+
+        my_if : if (pixel_red = unsigned(all_ones(C_BITS_RED))) then
+          pixel_green <= pixel_green + 1;
+
+          if (pixel_green = unsigned(all_ones(C_BITS_GREEN))) then
+            pixel_blue <= pixel_blue + 1;
+          end if;
+        end if my_if;
+      end if;
+
     end if;
   end process;
 
